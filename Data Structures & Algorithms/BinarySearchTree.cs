71dbd4fc33aa5410f9023f1a2dc18273670426f7
Hibbah Nadeem <hibbah1998@gmail.com>
2019-03-09 14:10:52 +0500

    Data Structures & Algorithms

---
+++ Data Structures & Algorithms/BinarySearchTree.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch_Tree
+{
+    class T_Node
+    {
+        public int data;
+        public T_Node left, right, root;
+        public  T_Node()
+        {
+            right = null;
+            root = null;
+            left = null;
+            data = 0;
+        }
+        public T_Node(int input)
+        {
+            data = input;
+            right = null;
+            left = null;
+            root = null;
+        }
+
+    }
+    class BinarySearchTree
+    {
+      public  T_Node startroot=new T_Node();
+       public BinarySearchTree()
+        {
+           
+        }
+        public void Insert(int val)
+        {
+            if ( startroot.data== 0)
+            {
+                startroot.data = val;
+
+            }
+            else
+            {
+                T_Node n = new T_Node(val);
+                T_Node temp = startroot;
+                while ((temp.left != null)||(temp.right!=null))
+                {
+                    if (val < temp.data)
+                    {
+                        if (temp.left == null) break;
+                        else
+                        {
+                                temp = temp.left;
+                                continue;
+                        }
+                    }
+                    else
+                    {
+                        if (temp.right == null) break;
+                        else
+                        {
+                            temp = temp.right;
+                            continue;
+                        }
+                       
+                    }
+                   
+                }
+                if ((val >temp.data))
+                {
+                    temp.right = n;
+                    n.root = temp;
+                }
+                else 
+                {
+                    temp.left = n;
+                    n.root = temp;
+                }
+            }
+        }
+       public T_Node Search(int val)
+        {
+            T_Node current = startroot;
+            while (current.data!=val)
+            {
+                if(val<current.data)
+                {
+                    current=current.left;
+                }
+                else
+                {
+                    current = current.right;
+                }
+                if(current==null)
+                {
+                    return null;
+                }
+            }
+            return current;
+        }
+        public T_Node Min(T_Node current)
+        {
+             //current = startroot;
+            while(current.left!=null)
+            {
+                current = current.left;
+            }
+            return current;
+        }
+        public T_Node Max()
+        {
+            T_Node current = startroot;
+            while (current.right != null)
+            {
+                current = current.right;
+            }
+            return current;
+        }
+        public T_Node Delete(T_Node root,int key)
+        { 
+            if (root == null) return root;
+            if (key < root.data)
+                root.left = Delete(root.left, key);
+            else if (key > root.data)
+                root.right = Delete(root.right, key);
+            else
+            {
+                if (root.left == null)
+                {
+                    T_Node temp = root.right;
+                    root.data = temp.data;
+                    return temp;
+                }
+                else if (root.right == null)
+                {
+                    T_Node temp = root.left;
+                    root.data = temp.data;
+                    return temp;
+
+                }
+                else
+                {
+
+                    T_Node temp = Min(root.right);
+                    root.data = temp.data;
+                    if (temp.right == null && temp.left == null)
+                    {
+                        if (temp.root.data < temp.data)
+                            temp.root.right = null;
+                        if (temp.root.data > temp.data)
+                            temp.root.left = null;
+                    }
+                }
+            }
+            return root;
+
+        }
+            public void Dispaly (T_Node root1)
+        {
+           if (root1==null)
+            {
+                return;
+            }
+           else
+            {
+                Dispaly(root1.left);
+                Console.WriteLine(root1.data);
+                Dispaly(root1.right);
+            }
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // BinarySearchTree bst = new BinarySearchTree();
+            // //T_Node n = new T_Node();
+            // Console.WriteLine("Enter any value for root");
+            // int a = int.Parse(Console.ReadLine());
+            // bst.Insert(a);
+            // string ans = "";
+            // Console.WriteLine("Do you wish to Continue?");
+            // ans = Console.ReadLine();
+            //ans= ans.ToUpper();
+            // if(ans=="YES")
+            // {
+            //     do
+            //     {
+            //         Console.WriteLine("Enter any number?");
+            //         a = int.Parse(Console.ReadLine());
+            //         bst.Insert(a);
+            //         Console.WriteLine("Do you wish to Continue?");
+            //         ans = Console.ReadLine();
+            //         ans=ans.ToUpper();
+            //     } while (ans == "YES");
+            // }
+            // Console.WriteLine("Output for inorder postfix");
+
+            // bst.Dispaly(bst.startroot);
+            // Console.WriteLine("Value to search");
+            // int n = int.Parse(Console.ReadLine());
+            // Console.WriteLine("After Search:{0}", bst.Search(n).data);
+            BinarySearchTree bst = new BinarySearchTree();
+            bst.Insert(10);
+            bst.Insert(5);
+            bst.Insert(12);
+            bst.Insert(11);
+            bst.Insert(18);
+            Console.WriteLine("Output for inorder postfix");
+            bst.Dispaly(bst.startroot);
+            bst.Delete(bst.startroot, 10);
+            Console.WriteLine("Output for inorder postfix after delete");
+            bst.Dispaly(bst.startroot);
+        }
+    }
+}
---
+++ Data Structures & Algorithms/CircularLinkedList.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleLinkList
+{
+    class D_Node
+    {
+        public int data;
+        public D_Node next, pre;
+        public D_Node()
+        {
+            next = null;
+            data = 0;
+        }
+        public D_Node(int input)
+        {
+            data = input;
+        }
+
+    }
+    class DC_LinkedList
+    {
+        private D_Node start = new D_Node(), end = new D_Node();
+        public int lenght;
+        public DC_LinkedList()
+        {
+            start.next = null;
+            lenght = 0;
+        }
+
+        public void inserAtBegng(int val)
+        {
+            if (start.next == null)
+            {
+                D_Node n = new D_Node(val);
+                start.next = n;
+                end.pre = n; end.next = start.next;
+                n.next = start.next;
+            }
+            else
+
+            {
+                D_Node temp;
+                D_Node n = new D_Node(val);
+                n.next = start.next;
+                temp = start.next;
+                temp.pre = n;
+                start.next = n;
+                end.next = start.next;
+                n.next = start.next;
+            }
+        }
+        public void insertAtEnd(int val)
+        {
+            if (start.next == null)
+            {
+                D_Node n = new D_Node(val);
+                start.next = n;
+                end.pre = n; end.next = start.next;
+                n.next = start.next;
+            }
+            else
+            {
+                D_Node n = new D_Node(val);
+                end.pre.next = n;
+                n.pre = end.pre;
+                end.pre = n; end.next = start.next;
+                n.next = start.next;
+            }
+        }
+        public void InsertATbetween(int inputval, int beforeValue)
+        {
+            bool flag = false; D_Node temp = start.next;
+            while (temp.next != null)
+            {
+                if (temp.data == beforeValue)
+                {
+                    if (temp.next == null) { insertAtEnd(inputval); flag = true; break; }
+                    else
+                    {
+                        D_Node n = new D_Node(inputval);
+                        n.next = temp.next;
+                        n.pre = temp;
+                        temp.next.pre = n;
+                        temp.next = n;
+                        flag = true; break;
+                    }
+                }
+                else { temp = temp.next; }
+            }
+
+        }
+        public void Display(DC_LinkedList li)
+        {
+            D_Node temp = start.next;
+            while (temp.next != start.next)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+            Console.WriteLine(temp.data);
+
+
+        }
+        public void DeleteBeg()
+        {
+            start.next = start.next.next;
+            start.next.pre = null;
+        }
+        public void DeleteEnd()
+        {
+            end.pre = end.pre.pre;
+            end.pre.next = null;
+
+        }
+        public D_Node DeleteFrom(int val)
+        {
+            D_Node n = null; bool IS_DEL = false;
+            D_Node temp = start.next;
+            while (temp.next != null)
+            {
+                if (temp.data == val)
+                {
+
+                    if (temp.next == end.pre) { n = temp.next; DeleteEnd(); IS_DEL = true; break; }
+                    else
+                    {
+                        n = temp.next;
+                        temp.next = temp.next.next;
+                        temp.next.pre = n.pre; IS_DEL = true; break;
+                    }
+                }
+                else
+                {
+                    temp = temp.next;
+                }
+            }
+
+            return n;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            DC_LinkedList li = new DC_LinkedList();
+            do
+            {
+                Console.WriteLine("Which option do you wish to choose?\n1)Insert At Beginning\n2)Insert At End" +
+                    "\n3)Insert After \n4)Delete From Beginning \n5)Delete From End \n6)Delete From Between \n7)Exit \n8)Display\n9)Sort");
+                a = int.Parse(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        Console.Write("Please enter a value to be inserted:");
+                        int val = int.Parse(Console.ReadLine()); li.inserAtBegng(val);
+                        break;
+                    case 2:
+                        Console.Write("Please enter a value to be inserted:");
+                        int eval = int.Parse(Console.ReadLine());
+                        li.insertAtEnd(eval);
+                        break;
+                    case 3:
+                        Console.Write("Please enter a value to be inserted:");
+                        int ival = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the value after which you wish to add?");
+                        int mval = int.Parse(Console.ReadLine()); li.InsertATbetween(ival, mval);
+                        break;
+                    case 4: li.DeleteBeg(); break;
+                    case 5: li.DeleteEnd(); break;
+                    case 6:
+                        Console.WriteLine("Entervalue:"); int i = int.Parse(Console.ReadLine());
+                        li.DeleteFrom(i); break;
+                    case 8: li.Display(li); break;
+
+                }
+            } while (a != 7);
+
+        }
+    }
+}
